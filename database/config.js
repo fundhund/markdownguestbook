02bec12b0aa359a12d4e3abaffcfb3bc4bf6fbaf
fundhund <marius.mielke@googlemail.com>
2022-02-03 12:38:02 +0100

    Added production database

--- database/config.js
+++ database/config.js
@@ -1,11 +1,20 @@
 const { Pool } = require('pg')
 
-const pool = new Pool({
-    user: 'postgres',
-    password: '1945',
-    database: 'guestbook',
-    host: 'localhost',
-    port: '5432',
-})
+const config = process.env.NODE_ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+    : {
+        user: 'postgres',
+        password: '1945',
+        database: 'guestbook',
+        host: 'localhost',
+        port: '5432',
+    }
+
+const pool = new Pool(config)
 
 module.exports = pool
